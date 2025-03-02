6f38968689801777f9e7b751b2598b15b522a1e6
TranVuHoang <tranvuhoangjr@gmail.com>
2025-03-02 22:29:02 +0700

    create home section

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*-------------------------------------
+# Type js
+-------------------------------------*/
+const typeHome = new Typed(".multiple-text", {
+    strings: ['Frontend Developer', 'AI Engineering', 'Bad Boy'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
