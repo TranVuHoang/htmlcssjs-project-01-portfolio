9c53f3d8c7578d2c34a506e40a278655d966453f
TranVuHoang <tranvuhoangjr@gmail.com>
2025-03-02 23:11:51 +0700

    create about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 # Type js
 -------------------------------------*/
 const typeHome = new Typed(".multiple-text", {
-    strings: ['Frontend Developer', 'AI Engineering', 'Bad Boy'],
+    strings: ['Frontend Developer', 'AI Engineering', 'Good Boy'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
